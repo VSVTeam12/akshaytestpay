4b2c316c545ec084ef9535710e5221a10c616056
VSVTeam12 <112758732+VSVTeam12@users.noreply.github.com>
2022-11-15 14:44:12 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '5556985713:AAE4x7NMMM3JuZ7ukqj-IpYDFG--qCm5QDc',
+    token: '5437381422:AAESL4AVB83xYHg3iNcenvDm-JxBSW4mxK0',
     admins: 5169514861],
     curr:'INR'
 }
